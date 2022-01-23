1772420bf3079818031952bb67135f055399ffca
Anatol Basharov <funky.Tolly@mail.ru>
2022-01-23 13:24:53 +0300

    init1

--- src/GeekBrains/lesson1/HomeWorkApp/HomeWorkApp.java
+++ src/GeekBrains/lesson1/HomeWorkApp/HomeWorkApp.java
@@ -38,6 +38,9 @@ public class HomeWorkApp {
         if (a >= b) {
             System.out.println("a >= b");
         } else {
+
+            System.out.println("a<b");
+            System.out.println("a<b");
             System.out.println("a<b");
         }
     }
